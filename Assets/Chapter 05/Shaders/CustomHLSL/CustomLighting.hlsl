4776d93e7c9c477620cd04cf3ea5adb523db7f27
Mina Pêcheux <mina.pecheux@gmail.com>
2022-12-06 11:40:06 +0100

    feat(core): add chapter 05 assets+code

---
+++ Assets/Chapter 05/Shaders/CustomHLSL/CustomLighting.hlsl
@@ -0,0 +1,50 @@
+﻿#ifndef CUSTOM_LIGHTING_INCLUDED
+#define CUSTOM_LIGHTING_INCLUDED
+
+#if defined(SHADERGRAPH_PREVIEW)
+#else
+#pragma multi_compile _ _MAIN_LIGHT_SHADOWS
+#pragma multi_compile _ _MAIN_LIGHT_SHADOWS_CASCADE
+#pragma multi_compile _ _ADDITIONAL_LIGHTS_VERTEX _ADDITIONAL_LIGHTS
+#pragma multi_compile_fragment _ _ADDITIONAL_LIGHT_SHADOWS
+#pragma multi_compile_fragment _ _SHADOWS_SOFT
+#pragma multi_compile_fragment _ _SCREEN_SPACE_OCCLUSION
+#pragma multi_compile _ LIGHTMAP_ON
+#pragma multi_compile _ LIGHTMAP_SHADOW_MIXING
+#endif
+
+void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color)
+{
+#if defined(SHADERGRAPH_PREVIEW)
+    Direction = float3(0.5, 0.5, 0);
+    Color = 1;
+#else
+	float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+
+    Light mainLight = GetMainLight(shadowCoord);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+#endif
+}
+
+void AdditionalLights_float(float3 WorldPosition, float3 WorldNormal, float3 WorldView, out float3 Diffuse)
+{
+    float3 diffuseColor = 0;
+    float4 White = 1;
+
+#if !defined(SHADERGRAPH_PREVIEW)
+    WorldNormal = normalize(WorldNormal);
+    WorldView = SafeNormalize(WorldView);
+    int pixelLightCount = GetAdditionalLightsCount();
+    for (int i = 0; i < pixelLightCount; ++i)
+    {
+        Light light = GetAdditionalLight(i, WorldPosition);
+        half3 attenuatedLightColor = light.color * (light.distanceAttenuation * light.shadowAttenuation);
+        diffuseColor += LightingLambert(attenuatedLightColor, light.direction, WorldNormal);
+    }
+#endif
+
+    Diffuse = diffuseColor;
+}
+
+#endif
